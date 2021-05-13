f832549c8b4934df8c92d9a9c1e0771cc6699adb
Gaku Sato <red.blues.quartet@gmail.com>
2021-05-13 18:23:21 +0900

    Chapter 6 finished

--- shop/shop_list.php
+++ shop/shop_list.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login'])==false) {
-    print 'ログインされていません';
-    print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
-    exit();
+if (isset($_SESSION['member_login'])==false) {
+    print 'ようこそゲスト様　';
+    print '<a href="member_login.html">会員ログイン</a><br>';
+    print '<br>';
 } else {
-    print $_SESSION['staff_name'];
-    print 'さんログイン中<br>';
+    print 'ようこそ';
+    print $_SESSION['member_name'];
+    print '様';
+    print '<a href="member_logout.php">ログアウト</a><br>';
     print '<br>';
 }
 ?>
-
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
@@ -34,8 +35,6 @@ if (isset($_SESSION['login'])==false) {
 
         print '商品一覧<br><br>';
 
-        print '<form method="POST" action="pro_branch.php">';
-
         while(true) {
 
             $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
@@ -44,25 +43,21 @@ if (isset($_SESSION['login'])==false) {
                 break;
             }
 
-            print '<input type="radio" name="procode" value="'.$rec['code'].'">';
+            print '<a href="shop_product.php?procode='.$rec['code'].'">';
             print $rec['name'].'---';
             print $rec['price'].'円';
+            print '</a>';
             print '<br>';
 
         }
+        print '<br>';
+        print '<a href="shop_cartlook.php">カートを見る</a><br>';
 
-        print '<input type="submit" name="disp" value="参照">';
-        print '<input type="submit" name="add" value="追加">';
-        print '<input type="submit" name="edit" value="修正">';
-        print '<input type="submit" name="delete" value="削除">';
-        print '</form>';
     } catch (Exception $e) {
         print 'ただいま障害により大変ご迷惑をお掛けしております。';
         exit();
     }
     ?>
     
-    <br>
-    <a href="../staff_login/staff_top.php">トップメニューへ</a>
 </body>
 </html>
